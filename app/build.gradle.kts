ae758128a17632f5a898ffdc9b8e45a3a8b20925
JoeriKaiser <joeri.kaiser@hotmail.com>
2026-08-16 10:34:00 +0200

    chore: bump to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "fr.luteal.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
